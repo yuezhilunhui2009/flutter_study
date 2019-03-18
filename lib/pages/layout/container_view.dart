7587394a052f66ee70ae524f5a3f482cbaebbcf7
yuezhilunhui2009 <yuezhilunhui2009@gmail.com>
2019-03-19 01:23:32 +0800

    feat: 组件示例页描述、代码、文档 3 tab

--- lib/pages/layout/container_view.dart
+++ lib/pages/layout/container_view.dart
@@ -1,15 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 
-class ContainerView extends StatelessWidget {
+/// Container Widget 介绍
+class ContainerDescription extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Container Widget 介绍'),
+    );
+  }
+}
+
+/// Container Widget 代码示例
+class ContainerCodeExample extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Container Widget 代码示例'),
+    );
+  }
+}
+
+/// Container Widget 官方文档
+class ContainerDocumentation extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return WebviewScaffold(
+      url: 'https://docs.flutter.io/flutter/widgets/Container-class.html',
+    );
+  }
+}
+
+class ContainerView extends StatefulWidget {
+  @override
+  _ContainerViewState createState() => _ContainerViewState();
+}
+
+class _ContainerViewState extends State<ContainerView> {
+  int _selectedIndex = 0;
+  final _widgetOptions = [
+    ContainerDescription(),
+    ContainerCodeExample(),
+    ContainerDocumentation(),
+  ];
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Container Widget'),
       ),
-      body: Container(
-        child: Text('container_view'),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.description), title: Text('描述')),
+          BottomNavigationBarItem(icon: Icon(Icons.code), title: Text('代码')),
+          BottomNavigationBarItem(icon: Icon(Icons.book), title: Text('文档')),
+        ],
+        currentIndex: _selectedIndex,
+        fixedColor: Colors.deepPurple,
+        onTap: _onItemTapped,
       ),
     );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 }
